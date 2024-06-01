cd4c0cf18924aaf3de4e5b01102cea13d4df43f7
HyuDeQueue <quanghuy01062004@gmail.com>
2024-06-01 13:42:01 +0700

    Reformating codes

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,12 +1,54 @@
-import React from 'react';
-import './Login.scss';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import diamondIcon from '../../assets/logodas.png';
 import signInWithGoogle from '../../utils/authUtils';
+import './Login.scss';
 
 const GoogleLoginComponent = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLoginSuccess = async (userInfo) => {
+    setUser(userInfo);
+    navigate('/');
+  };
+
+  const handleLoginFailure = (error) => {
+    console.error('Login Failed', error);
+  };
+
+  const login = async () => {
+    try {
+      const userInfo = await signInWithGoogle();
+      handleLoginSuccess(userInfo);
+    } catch (error) {
+      handleLoginFailure(error);
+    }
+  };
+
   return (
-    <div>
-      <h1>React Google Login</h1>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-header">
+          <Avatar className="avatar" sx={{ width: 100, height: 100 }}>
+            <img src={diamondIcon} alt="Diamond Icon" style={{ width: '100%', height: '100%' }} />
+          </Avatar>
+          <h1>DAS</h1>
+          <h2>We Valued Your Diamond!</h2>
+        </div>
+        {user ? (
+          <div className="user-info">
+            <h3>Welcome, {user.name}</h3>
+            <Avatar src={user.picture} alt={user.name} sx={{ width: 56, height: 56, margin: 'auto' }} />
+            <p>Email: {user.email}</p>
+          </div>
+        ) : (
+          <button className="login-button" onClick={login}>
+            Dùng tài khoản Google
+          </button>
+        )}
+      </div>
     </div>
   );
 };
