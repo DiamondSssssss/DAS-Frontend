95c20709cea726be8233d3869b22238e7a04a5a1
HyuDeQueue <quanghuy01062004@gmail.com>
2024-06-02 15:19:03 +0700

    fixing some

--- src/layout/CustomerLayout.jsx
+++ src/layout/CustomerLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Component/Header/Header";
 import Footer from "../Component/Footer/Footer";
+import "./CustomerLayout.css";
 
 function CustomerLayout() {
   return (
