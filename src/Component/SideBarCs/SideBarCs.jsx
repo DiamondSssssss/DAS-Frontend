653208c6f09d3940cc233a27c6af93257e7c5b7f
HyuDeQueue <quanghuy01062004@gmail.com>
2024-06-16 22:58:31 +0700

    fix the activeclassname

--- src/Component/SideBarCs/SideBarCs.jsx
+++ src/Component/SideBarCs/SideBarCs.jsx
@@ -6,28 +6,28 @@ function SideBarCs() {
       <NavLink
         to="/"
         className="flex items-center py-2 px-3 rounded transition duration-200 hover:bg-gray-100 mb-2 text-gray-800"
-        activeClassName="bg-gray-200"
+        activeclassname="bg-gray-200"
       >
         <span className="mr-2">🏠</span> Trang Chủ
       </NavLink>
       <NavLink
         to="/consultingstaff/assessmentrequest"
         className="flex items-center py-2 px-3 rounded transition duration-200 hover:bg-gray-100 mb-2 text-gray-800"
-        activeClassName="bg-gray-200"
+        activeclassname="bg-gray-200"
       >
         <span className="mr-2">📄</span> Yêu Cầu Dịch Vụ
       </NavLink>
       <NavLink
         to="/consultingstaff/assessmentpaper"
         className="flex items-center py-2 px-3 rounded transition duration-200 hover:bg-gray-100 text-gray-800"
-        activeClassName="bg-gray-200"
+        activeclassname="bg-gray-200"
       >
         <span className="mr-2">📋</span> Đơn Giám Định
       </NavLink>
       <NavLink
         to="/consultingstaff/assetsmentpaper"
         className="flex items-center py-2 px-3 rounded transition duration-200 hover:bg-gray-100 text-gray-800"
-        activeClassName="bg-gray-200"
+        activeclassname="bg-gray-200"
       >
         <span className="mr-2">📋</span> Giấy Giám Định
       </NavLink>
