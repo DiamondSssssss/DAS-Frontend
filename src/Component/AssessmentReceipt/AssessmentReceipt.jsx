c3982c4466d07fbd368fc6ad12d74ed4b96e3724
HyuDeQueue <quanghuy01062004@gmail.com>
2024-06-24 01:11:31 +0700

    Update AssessmentReceipt.jsx

--- src/Component/AssessmentReceipt/AssessmentReceipt.jsx
+++ src/Component/AssessmentReceipt/AssessmentReceipt.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./AssessmentRequestConsulting.css";
+import "../AssessmentRequestPage/AssessmentRequestConsulting.css";
 
 function AssessmentReceipt() {
   const navigate = useNavigate();
