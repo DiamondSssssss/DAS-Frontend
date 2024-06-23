890399516ace9ff65d8e3575239fb2892c48fd23
BayZm <kimngan500454@gmail.com>
2024-06-23 12:26:35 +0700

    kiem tra lai di

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Outlet/>
-  )
+    <div className="app-container">
+      <Outlet />
+    </div>
+  );
 }
 
-export default App
+export default App;
