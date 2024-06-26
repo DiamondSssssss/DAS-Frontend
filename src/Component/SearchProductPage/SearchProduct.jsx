149f33a452408e7ff2d2a13169a8cfe658ffd5f6
MINHTGSE161249 <minhtgse161249@fpt.edu.vn>
2024-06-26 18:28:18 +0700

    update css search diamond and service css

--- src/Component/SearchProductPage/SearchProduct.jsx
+++ src/Component/SearchProductPage/SearchProduct.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { Container, Form, Button, Alert } from 'react-bootstrap';
+import { Container, Form, Button, Alert, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './SearchProduct.css'; // Import the custom CSS file
 
-const ProductSearch = () => {
-    const [productCode, setProductCode] = useState('');
+const SearchProduct = () => {
+    const [diamondCode, setDiamondCode] = useState('');
+    const [gemstoneCode, setGemstoneCode] = useState('');
+    const [jadeCode, setJadeCode] = useState('');
+    const [pearlCode, setPearlCode] = useState('');
     const [searchResult, setSearchResult] = useState(null);
     const [imageData, setImageData] = useState(null); // State to hold base64 image data
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e, productCode) => {
         e.preventDefault();
 
         try {
@@ -32,25 +36,96 @@ const ProductSearch = () => {
     };
 
     return (
-        <Container>
-            <h1 className="mt-5">Tra cứu thông tin sản phẩm</h1>
-            <p>
+        <Container className="mt-5">
+            <h1 className="text-center">Tra cứu thông tin sản phẩm</h1>
+            <p className="text-center">
                 Đây là dịch vụ tra cứu của PNJL nhằm mục đích mang đến cho quý khách hàng sự tiện lợi, nhanh chóng và chính xác về những thông tin của sản phẩm trên bảng giám định đúng với những thông tin lưu trong cơ sở dữ liệu của PNJL.
             </p>
-            <Form onSubmit={handleSubmit} className="d-flex mt-4">
-                <Form.Group controlId="productCode" className="mr-2 flex-grow-1">
-                    <Form.Label>Mã giám định</Form.Label>
-                    <Form.Control
-                        type="text"
-                        value={productCode}
-                        onChange={(e) => setProductCode(e.target.value)}
-                        placeholder="Nhập mã giám định"
-                    />
-                </Form.Group>
-                <Button variant="primary" type="submit" className="align-self-end">
-                    Tra cứu
-                </Button>
-            </Form>
+            <div className="search-form bg-light p-4 rounded">
+                <Form onSubmit={(e) => handleSubmit(e, diamondCode)} className="mb-3">
+                    <h5>Kim Cương</h5>
+                    <Row>
+                        <Col md={8}>
+                            <Form.Group controlId="diamondCode">
+                                <Form.Control
+                                    type="text"
+                                    value={diamondCode}
+                                    onChange={(e) => setDiamondCode(e.target.value)}
+                                    placeholder="Nhập số sản phẩm"
+                                />
+                            </Form.Group>
+                        </Col>
+                        <Col md={4} className="align-self-end">
+                            <Button variant="primary" type="submit" block>
+                                Truy Xuất
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+                <Form onSubmit={(e) => handleSubmit(e, gemstoneCode)} className="mb-3">
+                    <h5>Đá Màu</h5>
+                    <Row>
+                        <Col md={8}>
+                            <Form.Group controlId="gemstoneCode">
+                                <Form.Control
+                                    type="text"
+                                    value={gemstoneCode}
+                                    onChange={(e) => setGemstoneCode(e.target.value)}
+                                    placeholder="Nhập số sản phẩm"
+                                />
+                            </Form.Group>
+                        </Col>
+                        <Col md={4} className="align-self-end">
+                            <Button variant="primary" type="submit" block>
+                                Truy Xuất
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+                <Form onSubmit={(e) => handleSubmit(e, jadeCode)} className="mb-3">
+                    <h5>Cẩm Thạch</h5>
+                    <Row>
+                        <Col md={8}>
+                            <Form.Group controlId="jadeCode">
+                                <Form.Control
+                                    type="text"
+                                    value={jadeCode}
+                                    onChange={(e) => setJadeCode(e.target.value)}
+                                    placeholder="Nhập số sản phẩm"
+                                />
+                            </Form.Group>
+                        </Col>
+                        <Col md={4} className="align-self-end">
+                            <Button variant="primary" type="submit" block>
+                                Truy Xuất
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+                <Form onSubmit={(e) => handleSubmit(e, pearlCode)}>
+                    <h5>Ngọc Trai</h5>
+                    <Row>
+                        <Col md={8}>
+                            <Form.Group controlId="pearlCode">
+                                <Form.Control
+                                    type="text"
+                                    value={pearlCode}
+                                    onChange={(e) => setPearlCode(e.target.value)}
+                                    placeholder="Nhập số sản phẩm"
+                                />
+                            </Form.Group>
+                        </Col>
+                        <Col md={4} className="align-self-end">
+                            <Button variant="primary" type="submit" block>
+                                Truy Xuất
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+                <p className="mt-3 text-muted">
+                    LƯU Ý QUAN TRỌNG: Bảng tham khảo này chỉ nhằm mục đích giúp xác nhận số kiểm định (Number) do người sử dụng dịch vụ này cung cấp tương ứng với số kiểm định được lưu trữ tại nguồn dữ liệu của PNJL. Bảng tham khảo này không phải là giấy bảo hành, đánh giá hay định giá viên kim cương đính kèm và cũng không được xem như là một bảng giám định gốc của PNJL.
+                </p>
+            </div>
             {searchResult && <Alert variant="info" className="mt-4">{searchResult}</Alert>}
             {imageData && (
                 <div className="mt-4">
--- src/Component/ServicePricePage/Service.jsx
+++ src/Component/ServicePricePage/Service.jsx
@@ -1,126 +1,202 @@
 import React from "react";
-import Frame from 'react-frame-component';
 import "./style.css";
 
 export const EvaluateService = () => {
     return (
         <div className="evaluate-service">
-            <div className="div-2">
-                <div className="overlap">
-                    <div className="evaluation-service">
-                        <p className="p">CÁC DỊCH VỤ GIÁM ĐỊNH KIM CƯƠNG HIỆN CÓ TẠI DAS</p>
-                        <div className="rectangle" />
-                    </div>
-                    <div className="frame-2">
-                        <div className="frame-3">
-                            <Frame className="frame-8">
-                                <div className="frame-content">
-                                    – Thời gian gửi thực hiện giám định tùy theo từng thời điểm gửi.<br />
-                                    – Số lượng không hạn chế. Bảng giá dịch vụ theo qui định.
-                                </div>
-                            </Frame>
-                            <Frame className="frame-4">
-                                <div className="frame-content">
-                                    – Thời gian thực hiện giám định trong 3 giờ làm việc tính từ lúc nhận sản phẩm vào.<br />
-                                    – Số lượng gửi tùy từng thời điểm. Bảng giá dịch vụ theo qui định.
-                                </div>
-                            </Frame>
-                            <Frame className="frame-5">
-                                <div className="frame-content">
-                                    – Thời gian thực hiện giám định trong 48 giờ làm việc tính từ lúc nhận sản phẩm vào.<br />
-                                    – Số lượng gửi tùy từng thời điểm. Bảng giá dịch vụ theo qui định.
-                                </div>
-                            </Frame>
-                            <Frame className="frame-6">
-                                <div className="frame-content">
-                                    – Thời gian gửi thực hiện giám định tùy theo từng thời điểm gửi.<br />
-                                    – Số lượng không hạn chế. Bảng giá dịch vụ theo qui định.
-                                </div>
-                            </Frame>
-                        </div>
-                        <Frame className="frame-7">
-                            <div className="frame-content">
-                                – Thời gian thực hiện giám định trong 3 giờ làm việc tính từ lúc nhận sản phẩm vào.<br />
-                                – Số lượng gửi tùy từng thời điểm. Bảng giá dịch vụ theo qui định.
-                            </div>
-                        </Frame>
-                        <Frame className="frame-9">
-                            <div className="frame-content">
-                                – Thời gian thực hiện giám định trong 48 giờ làm việc tính từ lúc nhận sản phẩm vào.<br />
-                                – Số lượng gửi tùy từng thời điểm. Bảng giá dịch vụ theo qui định.
-                            </div>
-                        </Frame>
-                    </div>
+            <div className="section">
+                <p className="header">CÁC DỊCH VỤ GIÁM ĐỊNH KIM CƯƠNG HIỆN CÓ TẠI PNJLAB</p>
+                <div className="table-container">
+                    <table className="service-table">
+                        <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>LOẠI DỊCH VỤ</th>
+                                <th>NỘI DUNG</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Giám định thường</td>
+                                <td>
+                                    <ul>
+                                        <li>Thời gian gửi thực hiện giám định tùy theo từng thời điểm gửi.</li>
+                                        <li>Số lượng không hạn chế. Bảng giá dịch vụ theo qui định.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Giám định nhanh 3h</td>
+                                <td>
+                                    <ul>
+                                        <li>Thời gian thực hiện giám định trong 3 giờ làm việc tính từ lúc nhận sản phẩm vào</li>
+                                        <li>Số lượng gửi tùy từng thời điểm. Bảng giá dịch vụ theo qui định.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Giám định nhanh 48h</td>
+                                <td>
+                                    <ul>
+                                        <li>Thời gian thực hiện giám định trong 48 giờ làm việc tính từ lúc nhận sản phẩm vào.</li>
+                                        <li>Số lượng gửi tùy từng thời điểm. Bảng giá dịch vụ theo qui định.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Niêm phong thường (Seal lại)</td>
+                                <td>
+                                    <ul>
+                                        <li>Thời gian gửi thực hiện giám định tùy theo từng thời điểm gửi.</li>
+                                        <li>Số lượng không hạn chế. Bảng giá dịch vụ theo qui định.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>Niêm phong (Seal lại nhanh 3h)</td>
+                                <td>
+                                    <ul>
+                                        <li>Thời gian thực hiện giám định trong 3 giờ làm việc tính từ lúc nhận sản phẩm vào.</li>
+                                        <li>Số lượng gửi tùy từng thời điểm. Bảng giá dịch vụ theo qui định.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>Niêm phong (Seal lại nhanh 48h)</td>
+                                <td>
+                                    <ul>
+                                        <li>Thời gian thực hiện giám định trong 48 giờ làm việc tính từ lúc nhận sản phẩm vào.</li>
+                                        <li>Số lượng gửi tùy từng thời điểm. Bảng giá dịch vụ theo qui định.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>Cấp lại giấy giám định</td>
+                                <td>
+                                    <ul>
+                                        <li>Thực hiện cấp lại giấy giám định theo yêu cầu khách hàng.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>Khắc mã số cạnh</td>
+                                <td>
+                                    <ul>
+                                        <li>Thực hiện khắc mã số cạnh trên viên đá theo yêu cầu.</li>
+                                        <li>Chỉ thực hiện khắc những viên đá có kích thước (size) trên 4.00mm.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-                <div className="overlap-2">
-                    <div className="standards">
-                        <p className="text-wrapper-2">TIÊU CHUẨN GIÁM ĐỊNH KIM CƯƠNG DAS</p>
-                        <div className="overlap-3">
-                            <div className="rectangle-2" />
-                            <div className="frame-wrapper">
-                                <div className="frame-10">
-                                    <Frame className="frame-11">
-                                        <div className="frame-content">Nội Dung</div>
-                                    </Frame>
-                                    <Frame className="frame-13">
-                                        <div className="frame-content">Kim cương thiên nhiên (Natural Diamond)</div>
-                                    </Frame>
-                                    <Frame className="frame-16">
-                                        <div className="frame-content">Từ 3.00mm trở lên.</div>
-                                    </Frame>
-                                    <Frame className="frame-17">
-                                        <div className="frame-content">Tất cả các dạng cắt mài.</div>
-                                    </Frame>
-                                    <Frame className="frame-18">
-                                        <div className="frame-content">
-                                            Thang tiêu chuẩn:<br />
-                                            Từ màu D đến Z
-                                        </div>
-                                    </Frame>
-                                    <Frame className="frame-19">
-                                        <div className="frame-content">
-                                            Thang tiêu chuẩn:<br />
-                                            FL, IF, VVS1-VVS2, VS1-VS2, SI1-SI2, I1-I2-I3.
-                                        </div>
-                                    </Frame>
-                                    <Frame className="frame-20">
-                                        <div className="frame-content">
-                                            Thang tiêu chuẩn:<br />
-                                            Excellent, Very Good, Good, Fair, Poor<br />
-                                            Chỉ thực hiện phân cấp với viên có dạng cắt mài (shape&cut): Round Brilliant.
-                                        </div>
-                                    </Frame>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <Frame className="frame-21">
-                        <div className="frame-content">Đo các thông số tỉ lệ cắt mài viên đá.</div>
-                    </Frame>
+            </div>
+            <div className="section">
+                <p className="header">TIÊU CHUẨN GIÁM ĐỊNH KIM CƯƠNG PNJLAB</p>
+                <div className="table-container">
+                    <table className="service-table">
+                        <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>TIÊU CHUẨN GIÁM ĐỊNH</th>
+                                <th>NỘI DUNG</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Loại đá</td>
+                                <td>Kim cương thiên nhiên (Natural Diamond)</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Kích thước (Measurements)</td>
+                                <td>Từ 3.00mm trở lên.</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Dạng cắt mài (Shape&cut)</td>
+                                <td>Tất cả các dạng cắt mài.</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Màu sắc (Color)</td>
+                                <td>
+                                    <ul>
+                                        <li>Thang tiêu chuẩn: Từ màu D đến Z</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>Độ tinh khiết (Clarity)</td>
+                                <td>
+                                    <ul>
+                                        <li>Thang tiêu chuẩn: FL, IF, VVS1-VVS2, VS1-VS2, SI1-SI2, I1-I2-I3.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>Cắt mài (Cut)</td>
+                                <td>
+                                    <ul>
+                                        <li>Thang tiêu chuẩn: Excellent, Very Good, Good, Fair, Poor</li>
+                                        <li>Chỉ thực hiên phân cấp với viên có dạng cắt mài (shape&cut): Round Brilliant.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>Tỉ lệ cắt mài (Proportions)</td>
+                                <td>Đo các thông số tỉ lệ cắt mài viên đá.</td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>Mài bóng (Polish) Đối xứng (Symmetry)</td>
+                                <td>
+                                    <ul>
+                                        <li>Thang tiêu chuẩn: Excellent, Very Good, Good, Fair, Poor</li>
+                                        <li>Không thực hiện phân cấp những viên có kích thước (size) từ 3.00 đến 3.99mm.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td>Phát quang (Fluorescence)</td>
+                                <td>
+                                    <ul>
+                                        <li>Thang tiêu chuẩn: None, Faint, Medium, Strong, Very Strong.</li>
+                                        <li>Không thực hiện phân cấp những viên có kích thước (size) từ 3.00 đến 3.99mm.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>10</td>
+                                <td>Niêm phong (Ép Seal)</td>
+                                <td>Tất cả viên đá được PNJLab thực hiện giám định.</td>
+                            </tr>
+                            <tr>
+                                <td>11</td>
+                                <td>Giấy giám định (Diamond Grading Report)</td>
+                                <td>
+                                    <ul>
+                                        <li>Phát hành Giấy giám định những viên có kích thước (size) trên 4.00mm.</li>
+                                        <li>Không phát hành Giấy giám định những viên có kích thước (size) từ 3.00 đến 3.99mm.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-                <Frame className="frame-22">
-                    <div className="frame-content">
-                        Thang tiêu chuẩn:<br />
-                        Excellent, Very Good, Good, Fair, Poor<br />
-                        Không thực hiện phân cấp những viên có kích thước (size) từ 3.00 đến 3.99mm.
-                    </div>
-                </Frame>
-                <Frame className="frame-23">
-                    <div className="frame-content">
-                        Thang tiêu chuẩn:<br />
-                        None, Faint, Medium, Strong, Very Strong.<br />
-                        Không thực hiện phân cấp những viên có kích thước (size) từ 3.00 đến 3.99mm.
-                    </div>
-                </Frame>
-                <Frame className="frame-24">
-                    <div className="frame-content">Tất cả viên đá được PNJLab thực hiện giám định.</div>
-                </Frame>
-                <Frame className="frame-25">
-                    <div className="frame-content">
-                        Phát hành Giấy giám định những viên có kích thước (size) trên 4.00mm.<br />
-                        Không phát hành Giấy giám định những viên có kích thước (size) từ 3.00 đến 3.99mm.
-                    </div>
-                </Frame>
             </div>
         </div>
     );
